dba91a1c12dee88f78a9332b4e4dd39d65bc5249
edwin-ux-dev <edwin.ux.dev@gmail.com>
2024-09-16 00:59:08 +0000

    Terminé eemplo de incremento

---
+++ ejm-incremento.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int arr[5] = {10,20,30,40,50};
+
+    int *ptr = arr;
+
+    printf("valor inicial: %d\n", *ptr);
+
+    *ptr++;
+
+    printf("valor despues de incremento: %d\n", *ptr);
+
+
+    return 0;
+}
